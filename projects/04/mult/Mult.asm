f0cf2b0469a1fa431b17cab3b5d5ce0642c0707b
Cedric Chee <cedric@invictusbyte.com>
2021-12-29 17:26:01 +0800

    implement a program that does multiplication
    Write a multiplication operation in Hack assembly language.

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,60 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// The Hack machine language doesn't have a multiplication operation.
+//
+// All we have is addition and subtraction. So we're faced with a challenge of
+// expressing a multiplication operation using addition and subtraction.
+
+// Computes RAM[2] = a * n
+// Example: RAM[2] = 2 * 3 = 2 + 2 + 2 = 6
+// Usage: put a number (a) in RAM[0] and another number (n) in RAM[1]
+
+// Declare and initialize variables
+  @R0
+  D=M
+  @a
+  M=D     // a = R0
+
+  @R1
+  D=M
+  @n
+  M=D     // n = R1
+
+  @i
+  M=0     // i = 0
+
+  @R2
+  M=0
+  @sum
+  M=0     // sum = 0
+
+(LOOP)
+  @i
+  D=M
+  @n
+  D=D-M
+  @STOP
+  D;JGE   // if i >= n goto STOP
+
+  @sum
+  D=M
+  @a
+  D=D+M
+  @sum
+  M=D     // sum = sum + a
+  @i
+  M=M+1   // i = i + 1
+  
+  @LOOP
+  0;JMP
+
+(STOP)
+  @sum
+  D=M
+  @R2
+  M=D     // RAM[2] = sum
+
+(END)
+  @END
+  0;JMP
